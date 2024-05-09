1c935a33ab59c3571a52138afddbe8f319a0e031
Ali <ali5555mohammadi@gmail.com>
2024-05-09 19:42:31 +0430

    create script.js file

---
+++ scripte.js
@@ -0,0 +1,10 @@
+const btnEl = document.getElementById("btn");
+const animecountinerEl = document.getElementById("anime-countiner");
+const animeimgEl = document.getElementById("anime_img");
+const artistEl = document.getElementById("artist");
+
+
+btnEl.addEventListener("click",function(){
+    
+    const respons= fetch("")
+})
